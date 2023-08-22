1110488459fb4eb52b982c7d056c4ea8430dde1e
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-08-22 22:38:46 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -6,13 +6,10 @@ sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_gener
 # Firewall
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/haproxy
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
+
 
 # Add packages
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/passwall
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
 # svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
