8bab505dc4ce72ab1ab92ca74489b3bd7204fb9f
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-08-27 16:20:52 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -7,9 +7,10 @@ sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_gener
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
 
-
 # Add packages
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
 # svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+
+# passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages package/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
