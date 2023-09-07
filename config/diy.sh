f5bc04c792c505cca5d23ccbcf7a7e980eeb4a1c
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-09-07 18:16:45 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
 
 # Firewall
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
