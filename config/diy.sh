e2de571ff287583a09b1abda73de671a53a63128
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-09-14 07:09:41 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd openwrt
-
 # Default IP
 # sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
 
@@ -17,10 +15,6 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
 # svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
 
 # passwall2
-# git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages package/passwall_package
-# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-# git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
-# svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages package/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+
