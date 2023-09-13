5df3f4a14415b35e3f4dc260ec9f59b8de8cd80a
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-09-13 14:42:48 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -14,14 +14,11 @@ wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-da
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
 # svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/haproxy
-rm -rf feeds/packages/net/v2ray-geodata
-
-# passwall
+# passwall2
 # git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages package/passwall_package
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+# git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 # svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
